664dcf0428ee65b402b1c5d3b03417562ed3f857
Alan Alberto Cruz Dominguez <alan@adomicil.io>
2021-10-07 17:20:35 -0500

    Created the route in MoviesController

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -17,7 +17,8 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
-        public ActionResult Edit(int id)
+        
+        public ActionResult Edit(int? id)
         {
             return Content("id = " + id);
         }
@@ -32,6 +33,7 @@ namespace Vidly.Controllers
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
